6a351e5eb7c0af767baaa562c799f87a945ebe15
angel quintero <zabaladuran123@gmail.com>
2024-11-20 14:14:56 -0500

    arreglos de diseño

--- inicio/home.php
+++ inicio/home.php
@@ -44,7 +44,7 @@ $graficaData = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Inventarios y Mantenimiento</title>
-    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard4.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> <!-- Iconos -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> <!-- Inclusión de Chart.js -->
 </head>
--- inicio/inventario.php
+++ inicio/inventario.php
@@ -98,7 +98,7 @@ $totalPrecio = 0; // Variable para almacenar el precio total
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventario</title>
-    <link rel="stylesheet" href="../css/inventario.css">
+    <link rel="stylesheet" href="../css/inventario44.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
